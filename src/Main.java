3a26614f6e37ce58162dbd72bed931eaf78561f9
helga <helga-nikol09@yandex.ru>
2023-06-01 03:23:50 +0300

    books and authors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import hw12objectsAndClasses.Author;
+import hw12objectsAndClasses.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author rowling = new Author("Джоан","Роулинг");
+        Author vern = new Author("Жюль", "Верн");
+        Book HP1 = new Book("Гарри Поттер и филосовский камень", rowling, 1997);
+        Book mysteriousIsland = new Book("Таинтвенный остров", vern, 1875);
+        mysteriousIsland.setPublishingYear(2016);
     }
 }
